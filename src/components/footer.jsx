5569a785eb06dc32cec0afc48a415cfd9df92751
David <elaman10@gmail.com>
2019-03-16 02:01:03 -0700

    Change footer to functional component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
-import { Navbar, Container, NavbarBrand } from "react-bootstrap";
 // Stateless Functional Component
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="fixed-bottom">
-        <p class="copyright text-muted text-center">
-          Made by David Young Lee 2019
-        </p>
-      </div>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <div className="fixed-bottom">
+      <p class="copyright text-muted text-center">
+        Made by David Young Lee 2019
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
