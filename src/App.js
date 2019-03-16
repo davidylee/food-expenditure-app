8145691902843664b4ebd2afda62c38580667f88
David <elaman10@gmail.com>
2019-03-15 21:08:35 -0700

    Initialize repository

--- src/App.js
+++ src/App.js
@@ -1,28 +1,129 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+import React, { Component } from "react";
 
+const foodexpenditureItems = [
+  {
+    id: 1,
+    date: "2019-03-13",
+    name: "Test Item 1",
+    price: 10
+  },
+  {
+    id: 2,
+    date: "2019-03-15",
+    name: "Test Item 2",
+    price: 12.0
+  }
+];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemList: foodexpenditureItems
+    };
+  }
+  renderItems = () => {
+    const newItems = this.state.itemList;
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span className={`foodexpenditure-title mr-2`} title={item.}>
+          {item.name}
+        </span>
+        <span>
+          <button className="btn btn-secondary mr-2"> Edit </button>
+          <button className="btn btn-danger">Delete </button>
+        </span>
+      </li>
+    ));
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">
+          Food Expenditure Tracking app
+        </h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add Item</button>
+              </div>
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 }
 
+export default App;
+*/
+
+import React, { Component } from "react";
+const foodexpenditureItems = [
+  {
+    id: 1,
+    date: "2019-03-13",
+    name: "Panera Small Coffee",
+    price: 2.39
+  },
+  {
+    id: 2,
+    date: "2019-03-15",
+    name: "Test Item 2",
+    price: 12.0
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemList: foodexpenditureItems
+    };
+  }
+  renderItems = () => {
+    const newItems = this.state.itemList;
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span className={`foodexpenditure-title mr-2`}>{item.date}</span>
+        <span className={`foodexpenditure-title mr-2`}>{item.name}</span>
+        <span className={`foodexpenditure-title mr-2`}>{item.price}</span>
+        <span>
+          <button className="btn btn-secondary mr-2"> Edit </button>
+          <button className="btn btn-danger">Delete </button>
+        </span>
+      </li>
+    ));
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-center my-4">
+          Food Expenditure Tracking App
+        </h1>
+        <div className="row">
+          <div className="col-md-12 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="add-item-button">
+                <button className="btn btn-primary">Add Item</button>
+              </div>
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
 export default App;
