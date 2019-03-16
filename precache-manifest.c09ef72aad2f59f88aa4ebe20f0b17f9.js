81f7f478851a33a65db5b2dc66415c578dcf56fb
David <elaman10@gmail.com>
2019-03-16 01:02:18 -0700

    Updates

---
+++ precache-manifest.c09ef72aad2f59f88aa4ebe20f0b17f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51b671529fbea7efbe80",
+    "url": "/food-expenditure-app/static/css/main.c5a4a9b8.chunk.css"
+  },
+  {
+    "revision": "51b671529fbea7efbe80",
+    "url": "/food-expenditure-app/static/js/main.e5b82ad8.chunk.js"
+  },
+  {
+    "revision": "0699760e9eb1f698466b",
+    "url": "/food-expenditure-app/static/js/runtime~main.229e8bb9.js"
+  },
+  {
+    "revision": "2321ef67ebd572c424ef",
+    "url": "/food-expenditure-app/static/css/2.50850118.chunk.css"
+  },
+  {
+    "revision": "2321ef67ebd572c424ef",
+    "url": "/food-expenditure-app/static/js/2.81b12289.chunk.js"
+  },
+  {
+    "revision": "0537f0aabd6921e8ab35fe3dd9917cd8",
+    "url": "/food-expenditure-app/index.html"
+  }
+];
